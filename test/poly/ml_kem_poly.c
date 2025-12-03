4b0bbaac65104c8aea6ab24a039dac8c05a1037d
Uaenasxbpy <3251316564@qq.com>
2025-12-03 16:01:16 +0800

    poly saw

--- test/poly/ml_kem_poly.c
+++ test/poly/ml_kem_poly.c
@@ -1,38 +1,38 @@
-/*
- * This file is part of the openHiTLS project.
- *
- * openHiTLS is licensed under the Mulan PSL v2.
- * You can use this software according to the terms and conditions of the Mulan PSL v2.
- * You may obtain a copy of Mulan PSL v2 at:
- *
- *     http://license.coscl.org.cn/MulanPSL2
- *
- * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
- * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
- * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
- * See the Mulan PSL v2 for more details.
- */
+#include <stdint.h>
 
-// #include "hitls_build.h"
-// #ifdef HITLS_CRYPTO_MLKEM
-#include "ml_kem_local.h"
+#define MLKEM_N        256
+#define MLKEM_N_HALF   128
+#define MLKEM_K_MAX    4
+#define MLKEM_Q        3329
+#define MLKEM_Q_INV_BETA (-3327)
+
+static inline int16_t BarrettReduction(int16_t a) {
+    const int16_t v = ((1 << 26) + MLKEM_Q / 2) / MLKEM_Q;
+    int16_t t = ((int32_t)v * a + (1 << 25)) >> 26;
+    t *= MLKEM_Q;
+    return a - t;
+}
+
+static inline int16_t MontgomeryReduction(int32_t a) {
+    int16_t t = (int16_t)a * MLKEM_Q_INV_BETA;
+    t = (a - (int32_t)t * MLKEM_Q) >> 16;
+    return t;
+}
 
 // basecase multiplication: add to polyH but not override it
-static void BaseMulAdd(int16_t polyH[2], const int16_t f0, const int16_t f1, const int16_t g0, const int16_t g1,
-                       const int16_t factor)
+static void BaseMulAdd(int16_t polyH[2], const int16_t f0, const int16_t f1,
+                       const int16_t g0, const int16_t g1, const int16_t factor)
 {
     polyH[0] += (int16_t)((f0 * g0 + f1 * g1 % MLKEM_Q * factor) % MLKEM_Q);
     polyH[1] += (int16_t)((f0 * g1 + f1 * g0) % MLKEM_Q);
 }
 
-static void CircMulAdd(int16_t dest[MLKEM_N], const int16_t src1[MLKEM_N], const int16_t src2[MLKEM_N],
-                       const int16_t *factor)
+static void CircMulAdd(int16_t dest[MLKEM_N], const int16_t src1[MLKEM_N],
+                       const int16_t src2[MLKEM_N], const int16_t *factor)
 {
     for (uint32_t i = 0; i < MLKEM_N / 4; i++) {
-        // 4-byte data is calculated in each round.
-        BaseMulAdd(&dest[4 * i], src1[4 * i], src1[4 * i + 1], src2[4 * i], src2[4 * i + 1], factor[i]);
-        BaseMulAdd(&dest[4 * i + 2], src1[4 * i + 2], src1[4 * i + 3], src2[4 * i + 2], src2[4 * i + 3],
-                   -1 * factor[i]);
+        BaseMulAdd(&dest[4 * i],     src1[4 * i],     src1[4 * i + 1], src2[4 * i],     src2[4 * i + 1], factor[i]);
+        BaseMulAdd(&dest[4 * i + 2], src1[4 * i + 2], src1[4 * i + 3], src2[4 * i + 2], src2[4 * i + 3], (int16_t)(-factor[i]));
     }
 }
 
@@ -42,8 +42,10 @@ static void PolyReduce(int16_t *poly)
         poly[i] = BarrettReduction(poly[i]);
     }
 }
+
 // polyVecOut += (matrix * polyVec): add to polyVecOut but not override it
-void MLKEM_MatrixMulAdd(uint8_t k, int16_t **matrix, int16_t **polyVec, int16_t **polyVecOut, const int16_t *factor)
+void MLKEM_MatrixMulAdd(uint8_t k, int16_t **matrix, int16_t **polyVec,
+                        int16_t **polyVecOut, const int16_t *factor)
 {
     int16_t **currOutPoly = polyVecOut;
     for (int i = 0; i < k; ++i) {
@@ -60,8 +62,8 @@ void MLKEM_MatrixMulAdd(uint8_t k, int16_t **matrix, int16_t **polyVec, int16_t 
 }
 
 // polyVecOut += (matrix^T * polyVec): add to polyVecOut but not override it
-void MLKEM_TransposeMatrixMulAdd(uint8_t k, int16_t **matrix, int16_t **polyVec, int16_t **polyVecOut,
-                                 const int16_t *factor)
+void MLKEM_TransposeMatrixMulAdd(uint8_t k, int16_t **matrix, int16_t **polyVec,
+                                 int16_t **polyVecOut, const int16_t *factor)
 {
     int16_t **currOutPoly = polyVecOut;
     for (int i = 0; i < k; ++i) {
@@ -76,8 +78,8 @@ void MLKEM_TransposeMatrixMulAdd(uint8_t k, int16_t **matrix, int16_t **polyVec,
     }
 }
 
-void MLKEM_VectorInnerProductAdd(uint8_t k, int16_t **polyVec1, int16_t **polyVec2, int16_t *polyOut,
-                                 const int16_t *factor)
+void MLKEM_VectorInnerProductAdd(uint8_t k, int16_t **polyVec1, int16_t **polyVec2,
+                                 int16_t *polyOut, const int16_t *factor)
 {
     for (int i = 0; i < k; ++i) {
         CircMulAdd(polyOut, polyVec1[i], polyVec2[i], factor + MLKEM_N_HALF / 2);
@@ -86,39 +88,38 @@ void MLKEM_VectorInnerProductAdd(uint8_t k, int16_t **polyVec1, int16_t **polyVe
 
 void MLKEM_SamplePolyCBD(int16_t *polyF, uint8_t *buf, uint8_t eta)
 {
-    uint32_t i;
-    uint32_t j;
-    uint8_t a;
-    uint8_t b;
+    uint32_t i, j;
+    uint8_t a, b;
     uint32_t t1;
     if (eta == 3) {  // The value of eta can only be 2 or 3.
         for (i = 0; i < MLKEM_N / 4; i++) {
             uint32_t temp = (uint32_t)buf[eta * i];
             temp |= (uint32_t)buf[eta * i + 1] << 8;
             temp |= (uint32_t)buf[eta * i + 2] << 16;
-            t1 = temp & 0x00249249;  // temp & 0x00249249 is used to obtain a specific bit in temp.
+            t1 = temp & 0x00249249;  // extract specific bits
             t1 += (temp >> 1) & 0x00249249;
             t1 += (temp >> 2) & 0x00249249;
 
             for (j = 0; j < 4; j++) {
                 a = (t1 >> (6 * j)) & 0x3;
                 b = (t1 >> (6 * j + eta)) & 0x3;
-                polyF[4 * i + j] = a - b;
+                polyF[4 * i + j] = (int16_t)(a - b);
             }
         }
     } else if (eta == 2) {
         for (i = 0; i < MLKEM_N / 4; i++) {
             uint16_t temp = (uint16_t)buf[eta * i];
-            temp |= (uint16_t)buf[eta * i + 1] << 0x8;
-            t1 = temp & 0x5555;  // temp & 0x5555 is used to obtain a specific bit in temp.
+            temp |= (uint16_t)buf[eta * i + 1] << 8;
+            t1 = temp & 0x5555;  // extract specific bits
             t1 += (temp >> 1) & 0x5555;
 
             for (j = 0; j < 4; j++) {
                 a = (t1 >> (4 * j)) & 0x3;
                 b = (t1 >> (4 * j + eta)) & 0x3;
-                polyF[4 * i + j] = a - b;
+                polyF[4 * i + j] = (int16_t)(a - b);
             }
         }
     }
 }
-// #endif
+
+// clang -O0 -g -emit-llvm -c ml_kem_poly.c -o ml_kem_poly.bc
