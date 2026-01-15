02302dd15177cd13c16f9cb47e1bb846f4ae33d2
Uaenasxbpy <3251316564@qq.com>
2026-01-15 14:09:10 +0800

    26.01.15

--- cryptol/ml_kem_pke/GenMatrix/GenMatrix.c
+++ cryptol/ml_kem_pke/GenMatrix/GenMatrix.c
@@ -1,8 +1,3 @@
-/*
- * GenMatrix.c
- * 零依赖版：内建了 memcpy/memset，无需 string.h
- */
-
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -34,7 +29,7 @@ void *memset(void *dst, int c, size_t n) {
 #define MLKEM_Q 3329
 #define MLKEM_K_MAX 4       
 #define MLKEM_SEED_LEN 32   
-#define MLKEM_XOF_OUTPUT_LENGTH 1000 
+// #define MLKEM_XOF_OUTPUT_LENGTH 1000 
 
 #define CRYPT_SUCCESS 0
 #define CRYPT_MLKEM_KEYLEN_ERROR (-1)
@@ -48,58 +43,70 @@ void *memset(void *dst, int c, size_t n) {
 // 3. 结构体定义
 
 typedef struct {
-    uint8_t k; 
-} CRYPT_MLKEM_Info;
+    uint8_t k;
+    uint8_t eta1;
+    uint8_t eta2;
+    uint8_t du;
+    uint8_t dv;
+    uint32_t secBits;
+    uint32_t encapsKeyLen;
+    uint32_t decapsKeyLen;
+    uint32_t cipherLen;
+    uint32_t sharedLen;
+    uint32_t bits;
+} CRYPT_MlKemInfo;
 
-typedef struct {
-    const CRYPT_MLKEM_Info *info;
-    void *libCtx; 
-} CRYPT_MLKEM_Ctx;
-
+struct CryptMlKemCtx {
+    int32_t algId;
+    const CRYPT_MlKemInfo *info;
+    uint8_t *ek;
+    uint32_t ekLen;
+    uint8_t *dk;
+    uint32_t dkLen;
+    // BSL_SAL_RefCount references;
+    void *libCtx;
+    // MLKEM_MatrixSt keyData;
+};
+typedef struct CryptMlKemCtx CRYPT_ML_KEM_Ctx;
+#define MLKEM_XOF_OUTPUT_LENGTH 578
 // 4. Mock 函数
 
 /**
  * [MOCK] HashFuncXOF
  * SAW 验证时会被 Override 替换，这里的实现仅为编译通过。
  */
-int32_t HashFuncXOF(void *libCtx, const uint8_t *in, uint32_t inLen, uint8_t *out, uint32_t outLen)
-{
-    if (out != (void*)0) {
-        memset(out, 0, outLen);
-    }
-    return CRYPT_SUCCESS;
-}
+int32_t HashFuncXOF(void *libCtx, const uint8_t *in, uint32_t inLen, uint8_t *out, uint32_t outLen);
 
 /**
  * Parse
  */
-int32_t Parse(uint16_t *polyNtt, uint8_t *arrayB, uint32_t arrayLen, uint32_t n)
-{
-    uint32_t i = 0;
-    uint32_t j = 0;
-    while (j < n) {
-        if (i + 3 > arrayLen) {
-            BSL_ERR_PUSH_ERROR(CRYPT_MLKEM_KEYLEN_ERROR);
-            return CRYPT_MLKEM_KEYLEN_ERROR;
-        }
-        uint16_t d1 = ((uint16_t)arrayB[i]) + (((uint16_t)arrayB[i + 1] & 0x0f) << 8);
-        uint16_t d2 = (((uint16_t)arrayB[i + 1]) >> 4) + (((uint16_t)arrayB[i + 2]) << 4);
-        if (d1 < MLKEM_Q) {
-            polyNtt[j] = d1;
-            j++;
-        }
-        if (d2 < MLKEM_Q && j < n) {
-            polyNtt[j] = d2;
-            j++;
-        }
-        i += 3;
-    }
-    return CRYPT_SUCCESS;
-}
+int32_t Parse(uint16_t *polyNtt, uint8_t *arrayB, uint32_t arrayLen, uint32_t n);
+// {
+//     uint32_t i = 0;
+//     uint32_t j = 0;
+//     while (j < n) {
+//         if (i + 3 > arrayLen) {
+//             BSL_ERR_PUSH_ERROR(CRYPT_MLKEM_KEYLEN_ERROR);
+//             return CRYPT_MLKEM_KEYLEN_ERROR;
+//         }
+//         uint16_t d1 = ((uint16_t)arrayB[i]) + (((uint16_t)arrayB[i + 1] & 0x0f) << 8);
+//         uint16_t d2 = (((uint16_t)arrayB[i + 1]) >> 4) + (((uint16_t)arrayB[i + 2]) << 4);
+//         if (d1 < MLKEM_Q) {
+//             polyNtt[j] = d1;
+//             j++;
+//         }
+//         if (d2 < MLKEM_Q && j < n) {
+//             polyNtt[j] = d2;
+//             j++;
+//         }
+//         i += 3;
+//     }
+//     return CRYPT_SUCCESS;
+// }
 
 // 5. 目标函数 GenMatrix
 
-int32_t GenMatrix(const CRYPT_MLKEM_Ctx *ctx, const uint8_t *digest,
+int32_t GenMatrix(const CRYPT_ML_KEM_Ctx *ctx, const uint8_t *digest,
                   int16_t *polyMatrix[MLKEM_K_MAX][MLKEM_K_MAX], bool isEnc)
 {
     uint8_t k = ctx->info->k;
